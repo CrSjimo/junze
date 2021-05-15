9c187d7130b05c721f42bc090fa2fa6ccca10a52
CrSjimo <crs_16423@outlook.com>
2021-05-15 20:16:58 +0800

    add mutiple generate

--- src/index.js
+++ src/index.js
@@ -10,6 +10,26 @@ function calcChar(){
     return String.fromCharCode(random.default.int(0x4e00,0x9fff));
 }
 
-const container = document.getElementById("junze");
-container.textContent = `俊泽不在的第${calcDate()}天，${calcChar()}他。`
+function generateText(times){
+    const container = document.getElementById("text-container");
+    const newContainer = document.createElement("div");
+    newContainer.id = "text-container";
+    let date = calcDate();
+    for(let i = 0;i<times;i++){
+        const textElem = document.createElement("h1");
+        const anchor = document.createElement("a");
+        let char = calcChar();
+        anchor.textContent = `俊泽不在的第${date}天，${char}他。`;
+        anchor.href = `https://www.baidu.com/baidu?ie=utf-8&wd=${char}`;
+        anchor.target = "_blank";
+        textElem.appendChild(anchor);
+        newContainer.appendChild(textElem);
+    }
+    container.replaceWith(newContainer);
+}
 
+document.getElementById("btn-once").onclick = ()=>generateText(1);
+document.getElementById("btn-4").onclick = ()=>generateText(4);
+document.getElementById("btn-8").onclick = ()=>generateText(8);
+
+generateText(1);
