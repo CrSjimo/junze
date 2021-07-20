345d3ffca141249a779b8c24312720ddf17a4830
CrSjimo <crs_16423@outlook.com>
2021-07-20 15:55:09 +0800

    add personalization

--- src/index.js
+++ src/index.js
@@ -4,12 +4,35 @@ const LeaveDatesMap = {
     junze_old: new Date("2021-04-28 00:00:00"),
     junze_new: new Date("2021-07-15 00:00:00"),
     cute_mb: new Date("2021-07-15 00:00:00"),
+    personalize: new Date("1919-08-10 00:00:00"),
+}
+
+const datePicker = document.getElementById("personalize-date");
+const personalizeText = document.getElementById("personalize-text");
+
+function getCurrentDate(){
+    if(currentMode=="personalize"){
+        return new Date(datePicker.value).getTime();
+    }else{
+        return LeaveDatesMap[currentMode] || LeaveDatesMap.junze_old;
+    }
+}
+
+function savePersonalization(){
+    if(currentMode!="personalize")return;
+    localStorage.setItem("personalize-date",datePicker.value);
+    localStorage.setItem("personalize-text",personalizeText.value);
+}
+
+function loadPersonalization(){
+    datePicker.value = localStorage.getItem("personalize-date") || "2021-04-28";
+    personalizeText.value = localStorage.getItem("personalize-text") || "俊泽不在的第%d天，%c他。";
 }
 
 let currentMode = "junze_old";
 
 function calcDate(){
-    const leaveDate = LeaveDatesMap[currentMode] || LeaveDatesMap.junze_old;
+    const leaveDate = getCurrentDate();
     const nowDate = new Date();
     return Math.floor((Number(nowDate) - Number(leaveDate))/(1000*86400));
 }
@@ -21,7 +44,14 @@ function calcChar(){
 const TextTemplate = {
     junze_old: (date,char)=>`俊泽不在的第${date}天，${char}他。`,
     junze_new: (date,char)=>`俊泽不在的第${date}天，${char}他。`,
-    cute_mb: (date,char)=>`看不见赵总的第${date}天，${char}她。`
+    cute_mb: (date,char)=>`看不见赵总的第${date}天，${char}她。`,
+    personalize: (date,char)=>{
+        return personalizeText.value
+            .replace(new RegExp("%d","g"),date)
+            .replace(new RegExp("%e","g"),-date)
+            .replace(new RegExp("%c","g"),char);
+        
+    },
 }
 
 function generateText(times){
@@ -41,7 +71,10 @@ function generateText(times){
         newContainer.appendChild(textElem);
     }
     container.replaceWith(newContainer);
+    savePersonalization();
 }
+
+const personalizeContainer = document.getElementById("personalize-container");
 
 function changeMode(mode,doApplyHash,doApplyRadio){
     if(!mode)mode="junze_old";
@@ -50,9 +83,15 @@ function changeMode(mode,doApplyHash,doApplyRadio){
     if(!LeaveDatesMap[currentMode])currentMode = "junze_old";
     if(doApplyHash)location.hash = currentMode;
     if(doApplyRadio)document.getElementById(currentMode).selected = true;
+    if(currentMode=="personalize"){
+        personalizeContainer.style.display="block";
+    }else{
+        personalizeContainer.style.display="none";
+    }
 }
 
 changeMode(location.hash,false,true);
+loadPersonalization();
 
 const select = document.getElementById("nav-container");
 select.onchange = ()=>{
