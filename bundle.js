e2436465132f769b7e3b210501e17aaa6bbaae03
CrSjimo <crs_16423@outlook.com>
2021-11-11 13:59:59 +0800

    fix time caluculation

--- bundle.js
+++ bundle.js
@@ -2182,7 +2182,7 @@ registries_1.functionRegistry.set('d', context => {
 });
 registries_1.functionRegistry.set('e', context => {
   if (!context.date) context.date = new Date('2021/04/28');
-  return Math.floor((Number(context.date) - Date.now()) / (1000 * 86400));
+  return -Math.floor((Date.now() - Number(context.date)) / (1000 * 86400));
 });
 registries_1.functionRegistry.set('j', (context, args) => {
   return juejuezi_1.default(args[0], args[1]);
